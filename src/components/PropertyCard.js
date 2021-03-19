6f4efca8e6490fda01a27e8718043ceafbfe0586
neelam618 <you@example.com>
2021-03-19 11:48:37 +0530

    badge addition & design in card

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -3,8 +3,11 @@ function PropertyCard(props) {
         <div className="bg-white rounded-lg overflow-hidden border">
             <img src={props.property.imageUrl} alt={props.property.imageAlt}></img>
             <div className="p-6">
-                <div className="text-gray-600 text-xs uppercase font-semibold tracking-wide">{props.property.beds} beds &bull; {props.property.baths} baths</div>
-                <h4 className="font-semibold text-lg leading-tight truncate">{props.property.title}</h4>
+                <div className="flex items-baseline">
+                    <span className="inline-block bg-green-200 text-green-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">New</span>
+                    <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">{props.property.beds} beds &bull; {props.property.baths} baths</div>
+                </div>
+                <h4 className="font-semibold text-lg leading-tight truncate mt-1">{props.property.title}</h4>
                 <div className="mt-1">{props.property.formattedPrice} <span className="text-gray-600 text-sm">/ wk</span></div>
                 <div className="mt-4">
                     <span className="text-green-600 font-semibold">{props.property.rating}/5 stars </span>

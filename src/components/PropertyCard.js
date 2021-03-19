fdfdff3bee80796df5a8f6dd2a3888491e39da54
neelam618 <you@example.com>
2021-03-19 10:24:31 +0530

    card styles

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,11 +1,13 @@
 function PropertyCard(props) {
     return (
-        <div>
+        <div className="bg-white rounded-lg overflow-hidden border">
             <img src={props.property.imageUrl} alt={props.property.imageAlt}></img>
-            <h4>{props.property.title}</h4>
-            <div>{props.property.beds} beds &bull; {props.property.baths} baths</div>
-            <div>{props.property.formattedPrice} / wk</div>
-            <div>{props.property.rating}/5 stars (based on {props.property.reviewCount} reviews)</div>
+            <div className="p-6">
+                <h4>{props.property.title}</h4>
+                <div>{props.property.beds} beds &bull; {props.property.baths} baths</div>
+                <div>{props.property.formattedPrice} / wk</div>
+                <div>{props.property.rating}/5 stars (based on {props.property.reviewCount} reviews)</div>
+            </div>
         </div>
     )
   }
